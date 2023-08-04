4b442f58af3f24f17a333132e420530464649413
chelloh <machelgabriel@gmail.com>
2023-08-04 14:36:22 +0300

    Update import path

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
+import App from "./main-page";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
