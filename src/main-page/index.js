dfabcd0afbedfcd63ed322c29fc1b6d0404956d3
chelloh <machelgabriel@gmail.com>
2023-08-04 23:02:18 +0300

    Update project

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,14 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Header from "./header";
 import "./main-page.css";
 
 function App() {
-	// load data
-	const userName = "Chelloh";
+	const [allHouses, setAllHouses] = useState([]);
+
+	useEffect(() => {
+		const fetchHouses = async () => {
+			const rsp = await fetch("/houses.json");
+			const houses = await rsp.json();
+			setAllHouses(houses);
+		};
+		fetchHouses();
+	}, []);
+
+	const featuredHouse = useMemo(() => {
+		if (allHouses.length) {
+			const randomIndex = Math.floor(Math.random() * allHouses.length);
+			return allHouses[randomIndex];
+		}
+	}, [allHouses]);
+
 	return (
-		<div className="container">
-			<Header subtitle={userName} />
-		</div>
+		<Router>
+			<div className="container">
+				<Header subtitle="Providing houses all over the world" />
+				<Routes>
+					<Route path="/"></Route>
+				</Routes>
+			</div>
+		</Router>
 	);
 }
 
