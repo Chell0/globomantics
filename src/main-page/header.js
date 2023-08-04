ad924bf91503844f5837450a314019299cce4ac3
chelloh <machelgabriel@gmail.com>
2023-08-04 15:04:17 +0300

    Add props in 'header.js'

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -1,13 +1,11 @@
 import logo from "./GloboLogo.png";
 
-const Header = () => (
+const Header = (props) => (
 	<header className="row">
 		<div className="col-md-5">
 			<img src={logo} className="logo" alt="logo" />
 		</div>
-		<div className="col-md-7 mt-5 subtitle">
-			Providing houses all over the world
-		</div>
+		<div className="col-md-7 mt-5 subtitle">{props.subtitle}</div>
 	</header>
 );
 
